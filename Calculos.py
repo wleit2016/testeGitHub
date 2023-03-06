f78f6387fa95097a9402aca1b92db09b01215752
wleit2016 <wenderleite2008@gmail.com>
2023-03-06 19:22:27 -0300

    Adicionando a funcao proximo numero no projeto.

--- Calculos.py
+++ Calculos.py
@@ -3,10 +3,21 @@ def ePrimo(numero):
     contar = 0
     indice = 2
 
-    while (indice <= numero):
+    while (indice <= numero and contar != 2):
         if(numero % indice == 0):
             contar = contar + 1
         indice = indice + 1
     if(contar == 1):
         resposta = True
     return resposta
+
+def proximoNumeroPrimo(numero):
+    numero = numero + 1
+    avancar = True
+
+    while(avancar):
+        if(not ePrimo(numero)):
+            numero = numero + 1
+        else:
+            avancar = False
+    return numero
--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from Calculos import ePrimo
+from Calculos import proximoNumeroPrimo
 
 if __name__ == '__main__':
     numero = int(input("Digite um numero: "))
     resultado = ePrimo(numero)
-    print(resultado)
 
-    print(type(True))
+    proximoNumero = proximoNumeroPrimo(numero)
+    print(resultado)
+    print("Proximo número: " + str(proximoNumero))
