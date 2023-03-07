e7049d18aab19310292fd54877d53813367c1038
wleit2016 <wenderleite2008@gmail.com>
2023-03-07 11:47:15 -0300

    foi feita modificacaoe para fazer a fatoracao dos numeros em primos

--- main.py
+++ main.py
@@ -2,9 +2,13 @@ from Calculos import ePrimo
 from Calculos import proximoNumeroPrimo
 
 if __name__ == '__main__':
+    numeroPrimo = 2
     numero = int(input("Digite um numero: "))
-    resultado = ePrimo(numero)
 
-    proximoNumero = proximoNumeroPrimo(numero)
-    print(resultado)
-    print("Proximo número: " + str(proximoNumero))
+    while numero != 1:
+        if numero % numeroPrimo == 0 :
+            print(numero, "|", numeroPrimo)
+            numero = int(numero / numeroPrimo)
+        else:
+            numeroPrimo = proximoNumeroPrimo(numeroPrimo)
+
