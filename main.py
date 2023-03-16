5f5666cdf10a453d46602c15a4a3ea285e3c023e
wleit2016 <wenderleite2008@gmail.com>
2023-03-16 11:28:31 -0300

    efetuando mudanca de nome na variavel numero e adicionando outra variavel

--- main.py
+++ main.py
@@ -2,15 +2,16 @@ from Calculos import proximoNumeroPrimo
 
 if __name__ == '__main__':
     numeroPrimo = 2
-    
-    numeroTXT = input("Digite um numero: ")
 
-    numero = int(numeroTXT)
+    numero1TXT = input("Digite um numero: ")
+    numero2TXT = input("Digite um numero: ")
 
-    while numero != 1:
-        if numero % numeroPrimo == 0 :
-            print(numero, "|", numeroPrimo)
-            numero = int(numero / numeroPrimo)
+    numero1 = int(numero1TXT)
+
+    while numero1 != 1:
+        if numero1 % numeroPrimo == 0 :
+            print(numero1, "|", numeroPrimo)
+            numero1 = int(numero1 / numeroPrimo)
         else:
             numeroPrimo = proximoNumeroPrimo(numeroPrimo)
 
